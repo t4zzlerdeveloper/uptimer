b61bb9723520291f4be324261ee0c74c08dbc373
t4zzlerdeveloper <38925744+t4zzlerdeveloper@users.noreply.github.com>
2024-03-09 13:42:54 +0000

    small changes

---
+++ src/lib/appwrite.js
@@ -0,0 +1,14 @@
+import { Client, Account, Databases, Functions,Avatars,Locale} from 'appwrite';
+
+export const client = new Client();
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(import.meta.env.VITE_PROJECT_ID);
+
+export const locale = new Locale(client);
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const functions = new Functions(client);
+export const avatars = new Avatars(client);
+export { ID } from 'appwrite';
